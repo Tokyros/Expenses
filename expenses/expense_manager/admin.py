01ec7cefcc4b89e782a10ac833ced60368836b70
Shahar Rosen <ps3tokyros@gmail.com>
2016-09-05 22:24:32 +0300

    Comment attemps2

--- expenses/expense_manager/admin.py
+++ expenses/expense_manager/admin.py
@@ -5,6 +5,7 @@ admin.site.register(models.Expense)
 admin.site.register(models.ExpenseType)
 admin.site.register(models.Portfolio)
 
+jhadskljfhasldkjfhasdf
 print("This is home")
 print("This is adding")
 print("This is home actually")
