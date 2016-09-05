f41dd44b1064cf1160236489ac762511b7ff3f62
Shahar Rosen <ps3tokyros@gmail.com>
2016-09-05 22:03:50 +0300

    Comment attemps

--- expenses/expense_manager/admin.py
+++ expenses/expense_manager/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from . import models
-
+# comment
 admin.site.register(models.Expense)
 admin.site.register(models.ExpenseType)
 admin.site.register(models.Portfolio)
